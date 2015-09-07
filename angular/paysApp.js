9638baf2651f3e511564ad8de965c52d4fabd4e6
Sasa Radovanovic <sasa1kg@yahoo.com>
2015-09-07 16:38:10 +0200

    translate added

--- angular/paysApp.js
+++ angular/paysApp.js
@@ -1,6 +1,6 @@
 var paysApp = angular.module("paysApp", ['ngRoute', 'ngCookies', 'LocalStorageModule', 
 	'GeoLocationService', 'CartService', 'WishlistService', 'SearchService', 
-	'ui-rangeSlider', 'cgBusy', 'brantwills.paging'])
+	'ui-rangeSlider', 'cgBusy', 'brantwills.paging', 'pascalprecht.translate'])
     .filter('html',function($sce){
     return function(input){
         console.log("AAAA".concat(input));
@@ -11,4 +11,86 @@ var paysApp = angular.module("paysApp", ['ngRoute', 'ngCookies', 'LocalStorageMo
 paysApp.config(function (localStorageServiceProvider) {
   localStorageServiceProvider
     .setPrefix('paysApp');
+});
+
+
+paysApp.run(function($rootScope, $translate) {
+        $rootScope.translate = function(lang) {
+			   $translate.use(lang);
+        };
+});
+
+paysApp.config(function($translateProvider) {
+    $translateProvider.useSanitizeValueStrategy(null);
+    $translateProvider.translations('en', {
+      HOME: 'Home',
+      SHOP: 'Shop',
+      BLOG: 'Blog',
+      FOR_FARMERS: 'For farmers',
+      FOR_DISTRIBUTORS : 'For distributors',
+      SEARCH : 'Search',
+      NEAR_YOU: 'Near you',
+      IN_CIRCLE_OF: 'In circle of',
+      CHOOSE_DISTANCE: 'Select a distance',
+      VIEW_OFFER: 'View Offer',
+      ACCOUNT: 'Account',
+      WISHLIST: 'Wishlist',
+      SHOPPING: 'Shopping',
+      CART: 'Cart',
+	  LOGIN: 'Log in',
+	  CONTACT : 'Contact us',
+	  PRODUCTS : 'Products',
+	  PRODUCT_DETAILS : 'Product details',
+	  BLOG_LIST : 'Blog list',
+	  BLOG_ONE : 'One blog',
+	  SEARCH_RESET : 'Reset search parameters',
+    YOUR_LOCATION : 'Your location',
+    CATEGORY : 'Category',
+    LOCATION_PLACE : 'Location',
+    MARKETING_SPACE : 'Advertising space',
+    ADVERTISING : 'Advertising',
+    MOST_IN_YEAR: 'The most sales in a year',
+    MOST_DIFFERENT: 'The most different products',
+    MOST_ORDERS: 'The most orders',
+    ABOUT_US: 'About us',
+    TITLE_1 : 'Everyday items shop',
+    TITLE_2 : 'direct from farmers',
+    TITLE_3 : 'Follow our blog'
+    })
+    .translations('rs', {
+      HOME: 'Početna',
+      SHOP: 'Prodavnica',
+      BLOG: 'Blog',
+      FOR_FARMERS: 'Za prodavce',
+      FOR_DISTRIBUTORS : 'Za distributere',
+      SEARCH : 'Pretraga',
+      NEAR_YOU: 'U vašoj okolini',
+      IN_CIRCLE_OF: 'U krugu od',
+      CHOOSE_DISTANCE: 'Izaberi udaljenost',
+      VIEW_OFFER: 'Pogledaj ponudu',
+      ACCOUNT: 'Nalog',
+      WISHLIST: 'Lista želja',
+      SHOPPING: 'Kupovina',
+      CART: 'Kolica',
+	  LOGIN: 'Ulaz',
+	  CONTACT : 'Kontaktirajte nas',
+	  PRODUCTS : 'Proizvodi',
+	  PRODUCT_DETAILS : 'Detalji proizvoda',
+	  BLOG_LIST : 'Lista blogova',
+	  BLOG_ONE : 'Jedan blog',
+	  SEARCH_RESET : 'Poništi izbor za pretragu',
+    YOUR_LOCATION : 'Vaša lokacija',
+    CATEGORY : 'Kategorija',
+    LOCATION_PLACE : 'Mesto',
+    MARKETING_SPACE : 'Reklamni prostor',
+    ADVERTISING : 'Reklame',
+    MOST_IN_YEAR: 'Najviše prodali u toku godine',
+    MOST_DIFFERENT: 'Najviše različitih proizvoda',
+    MOST_ORDERS: 'Najviše porudžbina',
+    ABOUT_US: 'O nama',
+    TITLE_1 : 'Kupovina svakodnevnih namirnica',
+    TITLE_2 : 'direktno od proizvođača',
+    TITLE_3 : 'Pratite naš blog'
+    });
+    $translateProvider.preferredLanguage('en');
 });
