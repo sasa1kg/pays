cdf6337cbf48ea13a8ecd6d3deaf7022df11a4da
nignjatov <ignjatov90@hotmail.com>
2016-01-19 22:14:15 +0100

    Alignment for i18n

--- angular/paysApp.js
+++ angular/paysApp.js
@@ -42,9 +42,19 @@ paysApp.config(function (localStorageServiceProvider) {
 
 
 paysApp.run(function ($rootScope, $translate, $location, $window, $filter, Notification, SearchService, UserService) {
+
+  $rootScope.englishLangCode = "en_EN";
+  $rootScope.serbianLangCode = "rs_RS";
+
+  $rootScope.currentLang = $rootScope.englishLangCode;
+
   $rootScope.translate           = function (lang) {
+    $rootScope.currentLang = lang;
     $translate.use(lang);
   };
+
+  $rootScope.translate($rootScope.currentLang);
+
   $rootScope.lastPage            = "#/";
   $rootScope.credentials         = UserService.getUserCredentials();
   $rootScope.$on('$routeChangeStart', function (event, next) {
@@ -140,7 +150,7 @@ paysApp.run(function ($rootScope, $translate, $location, $window, $filter, Notif
 
 paysApp.config(function ($translateProvider) {
   $translateProvider.useSanitizeValueStrategy(null);
-  $translateProvider.translations('en', {
+  $translateProvider.translations('en_EN', {
     HOME: 'Home',
     SHOP: 'Shop',
     BLOG: 'Blog',
@@ -416,20 +426,20 @@ paysApp.config(function ($translateProvider) {
     BANNER_IMAGE_UPLOADED: "Banner image uploaded",
     BANNER_IMAGE_FAILURE: "Failed to upload banner image",
     UPLOAD: "Upload",
-    PASSWORD_EMAIL_SENT : 'E-Mail containing link for your password reset is sent to request e-mail address',
-    PASSWORD_EMAIL_NOT_SENT : 'Failed to send e-mail for password reset',
-    FORGOT_PASS : 'Forgotten password?',
-    SEND_EMAIL : 'Send e-mail',
-    RESET_PASSWORD : 'Reset password',
-    PASSWORD_CHANGED : 'Password successfully changed',
-    PASSWORD_NOT_CHANGED : 'Failed to change password',
-    ENTER_NEW_PASSWORD : 'Please enter you new password',
-    PASSWORD_CHANGED_MSG : 'You have successfully chaned your password. By clicking on OK you will be redirected to login page where you can access your profile.',
-    ACTIVATED_USER_MSG : 'You have successfully activated your account. Please visit login page where you can access your profile.',
-    NOT_ACTIVATED_USER_MSG : 'Account activation failed. Please contact our support team to resolve this issue.'
+    PASSWORD_EMAIL_SENT: 'E-Mail containing link for your password reset is sent to request e-mail address',
+    PASSWORD_EMAIL_NOT_SENT: 'Failed to send e-mail for password reset',
+    FORGOT_PASS: 'Forgotten password?',
+    SEND_EMAIL: 'Send e-mail',
+    RESET_PASSWORD: 'Reset password',
+    PASSWORD_CHANGED: 'Password successfully changed',
+    PASSWORD_NOT_CHANGED: 'Failed to change password',
+    ENTER_NEW_PASSWORD: 'Please enter you new password',
+    PASSWORD_CHANGED_MSG: 'You have successfully chaned your password. By clicking on OK you will be redirected to login page where you can access your profile.',
+    ACTIVATED_USER_MSG: 'You have successfully activated your account. Please visit login page where you can access your profile.',
+    NOT_ACTIVATED_USER_MSG: 'Account activation failed. Please contact our support team to resolve this issue.'
 
   })
-    .translations('rs', {
+    .translations('rs_RS', {
       HOME: 'Početna',
       SHOP: 'Prodavnica',
       BLOG: 'Blog',
@@ -705,17 +715,17 @@ paysApp.config(function ($translateProvider) {
       BANNER_IMAGE_UPLOADED: "Postavljena nova reklamna slika",
       BANNER_IMAGE_FAILURE: "Neuspešno postavljanje nove reklamne slike",
       UPLOAD: "Postavi",
-      PASSWORD_EMAIL_SENT : 'E-Mail poruka sa linkom sa promenu Vaše lozinke je poslata na željenu e-mail adresu',
-      PASSWORD_EMAIL_NOT_SENT : 'Neuspešno slanje e-mail poruke za promenu lozinke',
-      FORGOT_PASS : 'Zaboravljena lozinka?',
-      SEND_EMAIL : 'Pošalji e-mail',
-      RESET_PASSWORD : 'Promeni lozinku',
-      PASSWORD_CHANGED : 'Lozinka uspešno promenljena',
-      PASSWORD_NOT_CHANGED : 'Neuspešna promena lozinke',
-      ENTER_NEW_PASSWORD : 'Molimo unesite novu lozinku',
-      PASSWORD_CHANGED_MSG : 'Uspešno ste promenili Vašu lozinku. Klikom na OK bićete prebačeni na stranicu za prijavu gde možete pristupiti Vašem profilu',
-      ACTIVATED_USER_MSG : 'Uspešno ste aktivirali Vaš nalog. Posetite stranicu za prijavu gde možete pristupiti Vašem profilu',
-      NOT_ACTIVATED_USER_MSG : 'Neuspešna aktivacija naloga. Molimo Vas da kontaktirate našu tehničku podršku radi rešavanja ovog problema.'
+      PASSWORD_EMAIL_SENT: 'E-Mail poruka sa linkom sa promenu Vaše lozinke je poslata na željenu e-mail adresu',
+      PASSWORD_EMAIL_NOT_SENT: 'Neuspešno slanje e-mail poruke za promenu lozinke',
+      FORGOT_PASS: 'Zaboravljena lozinka?',
+      SEND_EMAIL: 'Pošalji e-mail',
+      RESET_PASSWORD: 'Promeni lozinku',
+      PASSWORD_CHANGED: 'Lozinka uspešno promenljena',
+      PASSWORD_NOT_CHANGED: 'Neuspešna promena lozinke',
+      ENTER_NEW_PASSWORD: 'Molimo unesite novu lozinku',
+      PASSWORD_CHANGED_MSG: 'Uspešno ste promenili Vašu lozinku. Klikom na OK bićete prebačeni na stranicu za prijavu gde možete pristupiti Vašem profilu',
+      ACTIVATED_USER_MSG: 'Uspešno ste aktivirali Vaš nalog. Posetite stranicu za prijavu gde možete pristupiti Vašem profilu',
+      NOT_ACTIVATED_USER_MSG: 'Neuspešna aktivacija naloga. Molimo Vas da kontaktirate našu tehničku podršku radi rešavanja ovog problema.'
     })
-  $translateProvider.preferredLanguage('en');
+  $translateProvider.preferredLanguage('en_EN');
 });
