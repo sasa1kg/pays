97d9c95665eea2df16a30ffa22c05e8503ea20e4
Sasa Radovanovic <sasa1kg@yahoo.com>
2016-02-09 09:32:12 +0100

    Floating Cart link

---
+++ angular/controllers/floatingDiv.dir.js
@@ -0,0 +1,19 @@
+angular.module('paysApp').directive('setClassWhenAtTop', function ($window) {
+    var $win = angular.element($window); // wrap window object as jQuery object
+
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var topClass = attrs.setClassWhenAtTop, // get CSS class from directive's attribute value
+                offsetTop = element.offset().top + 70; // get element's top relative to the document
+
+            $win.on('scroll', function (e) {
+                if ($win.scrollTop() >= offsetTop) {
+                    element.addClass(topClass);
+                } else {
+                    element.removeClass(topClass);
+                }
+            });
+        }
+    };
+});
